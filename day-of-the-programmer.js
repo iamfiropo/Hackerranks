0c56e1327d070c8d71d4efa4d74303856819f663
Ropo Olatujoye <ropo@nairabox.com>
2020-06-03 04:34:26 +0100

    day of the programmer solution

---
+++ day-of-the-programmer.js
@@ -0,0 +1,24 @@
+/**
+ * check if year is lesser than 1918
+ *  check if year%4 === 0
+ *    print 12.09.year
+ *  else:
+ *    print 13.09.year
+ * else
+ *  check if (year%400 === 0) || (year%4 === 0 && (year%100 !== 0))
+ *    print 12.09.year
+ *  else:
+ *    print 13.09.year
+ */
+function dayOfProgrammer(year) {
+  if (year === 1918) {
+    return '26.09.' + year
+  }
+  if (year < 1918 && year % 4 === 0 || (year%400 === 0) || (year%4 === 0 && (year%100 !== 0))) {
+    return '12.09.' + year
+  } else {
+    return '13.09.' + year
+  }
+}
+
+dayOfProgrammer(2016)
